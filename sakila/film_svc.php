14cfa04b8ac3e487e484627da8dab70b960a6e37
wawaida <che.wahida@gmail.com>
2019-02-27 10:26:08 +0800

    service

--- sakila/film_svc.php
+++ sakila/film_svc.php
@@ -20,13 +20,19 @@ if(! $con){
 // query data
 $sql = "SELECT * FROM film LIMIT 0,20";
 $rs = mysqli_query($con, $sql);
+$arr = [];
 while($row = mysqli_fetch_array($rs)){
-    echo $row['title'].'<br>';
+    //echo $row['title'].'<br>';
+    $obj = new stdClass();
+    $obj->title = $row['title'];
+    $obj->desc = $row['description'];
+    $arr[] = $obj;
 }
 
 
-// query data
-
-
 // print data sbg json
+// {}/[] - json_encode() vs json_decode()
+$json = json_encode($arr);
+header('Content-type:application/json');
+echo $json;
 
